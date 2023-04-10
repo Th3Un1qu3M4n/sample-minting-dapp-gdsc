d5f4739c4b41d2888fb95df6e70d75d60dea9e41
Muhammad Ahmed <ahmed495802@gmail.com>
2023-04-11 04:18:49 +0500

    shifted to 433 ssl

--- src/mintNFT.js
+++ src/mintNFT.js
@@ -64,7 +64,7 @@ const WalletCard = () => {
     data.append("minter", accountString)
     
     // fetch('http://localhost:5500/mintNFT', {
-      fetch('https://34.222.45.192:5500/mintNFT', {
+      fetch('https://34.222.45.192/mintNFT', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({minter: accountString}),
